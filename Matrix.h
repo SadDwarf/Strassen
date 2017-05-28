34014e21f5382c69a8adcfd47f11a12081d7f232
tavplubix <tavplubix@asus>
2017-05-28 18:39:15 +0300

    many fixes

---
+++ Matrix.h
@@ -0,0 +1,148 @@
+#pragma once
+
+#include <stdexcept>
+#include <iostream>
+
+template <typename Num>
+class Matrix {
+private:
+    std::vector<Num> array;
+    size_t m_size;
+
+    Matrix quarter(int quarter) const {
+        Matrix c(size() / 2);
+        int i, j;
+        switch (quarter) {
+            case 1:
+                i = 0;          j = 0;          break;
+            case 2:
+                i = size() / 2; j = 0;          break;
+            case 3:
+                i = 0;          j = size() / 2; break;
+            case 4:
+                i = size() / 2; j = size() / 2; break;
+            default:
+                throw std::out_of_range("quarter = " + std::to_string(quarter));
+        }
+        for (size_t l = 0; l < size() / 2; ++l)
+            for (size_t k = 0; k < size() / 2; ++k)
+                c(l, k) = (*this)(i + l, j + k);
+        return c;
+    }
+
+    Matrix simple_multiplication(const Matrix& b) const {
+        Matrix c(size());
+        for (size_t i = 0; i < size(); ++i)
+            for (size_t j = 0; j < size(); ++j)
+                for (size_t k = 0; k < size(); ++k)
+                    c(i, j)+= (*this)(k, j) * b(i, k);
+        return c;
+    }
+
+    void check_size(const Matrix& b) const {
+        if (size() != b.size())
+            throw std::invalid_argument("this->size = " + std::to_string(size()) +
+                                        " other.size = " + std::to_string(b.size()));
+    }
+    void check_range(size_t i, size_t j) const {
+        if (m_size <= i || m_size <= j)
+            throw std::out_of_range("size = " + std::to_string(m_size) +
+                                    " i = " + std::to_string(i) +
+                                    " j = " + std::to_string(j));
+    }
+
+public:
+    Matrix(size_t size) {
+        double k = std::log2(size);
+        if (int(k) != k) {
+            m_size = std::pow(2, int(k) + 1);
+        } else
+            m_size = std::pow(2, int(k));
+        array.resize(m_size * m_size);
+    }
+
+    Matrix(const Matrix& a, const Matrix& b, const Matrix& c, const Matrix& d) {
+        m_size = a.size() * 2;
+        array.resize(m_size * m_size);
+        for (size_t i = 0; i < a.size(); ++i)
+            for (size_t j = 0; j < a.size(); ++j) {
+                (*this)(i,            j           ) = a(i, j);
+                (*this)(i + a.size(), j           ) = b(i, j);
+                (*this)(i,            j + a.size()) = c(i, j);
+                (*this)(i + a.size(), j + a.size()) = d(i, j);
+            }
+    }
+
+    int & operator () (size_t i, size_t j) {
+        check_range(i, j);
+        return array[i * m_size + j];
+    }
+
+    const int & operator() (size_t i, size_t j) const {
+        check_range(i, j);
+        return array[i * m_size + j];
+    }
+
+    size_t size() const {
+        return m_size;
+    }
+
+    Matrix operator* (const Matrix& b) const {
+        check_size(b);
+        if (size() <= 2)
+            return simple_multiplication(b);
+        Matrix m1 = (quarter(1) + quarter(4)) * (b.quarter(1) + b.quarter(4));
+        Matrix m2 = (quarter(3) + quarter(4)) * (b.quarter(1)               );
+        Matrix m3 = (quarter(1)             ) * (b.quarter(2) - b.quarter(4));
+        Matrix m4 = (quarter(4)             ) * (b.quarter(3) - b.quarter(1));
+        Matrix m5 = (quarter(1) + quarter(2)) * (b.quarter(4)               );
+        Matrix m6 = (quarter(3) - quarter(1)) * (b.quarter(1) + b.quarter(2));
+        Matrix m7 = (quarter(2) - quarter(4)) * (b.quarter(3) + b.quarter(4));
+
+        return Matrix(m1 + m4 - m5 + m7, m3 + m5, m2 + m4, m1 - m2 + m3 + m6);
+    }
+
+    Matrix& operator+= (const Matrix& a) {
+        check_size(a);
+        for (size_t i = 0; i < a.size(); ++i)
+            for (size_t j = 0; j < a.size(); ++j)
+                (*this)(i, j) += a(i, j);
+        return *this;
+    }
+
+    Matrix operator+ (const Matrix& b) const {
+        Matrix a = *this;
+        return a += b;
+    }
+
+    Matrix& operator-= (const Matrix& a) {
+        check_size(a);
+        for (size_t i = 0; i < a.size(); ++i)
+            for (size_t j = 0; j < a.size(); ++j)
+                (*this)(i, j) -= a(i, j);
+        return *this;
+    }
+
+    Matrix operator- (const Matrix& b) const {
+        Matrix a = *this;
+        return a -= b;
+    }
+};
+
+template <typename Num>
+std::ostream& operator << (std::ostream& out, const Matrix<Num>& m) noexcept {
+    for (size_t i = 0; i < m.size(); ++i) {
+        for (size_t j = 0; j < m.size(); ++j)
+            out << m(i, j) << ' ';
+        out << '\n';
+    }
+    return out;
+}
+
+template <typename Num>
+std::istream& operator >> (std::istream& in, Matrix<Num>& m) noexcept {
+    for (size_t i = 0; i < m.size(); ++i)
+        for (size_t j = 0; j < m.size(); ++j)
+            in >> m(i, j);
+    return in;
+}
--- main.cpp
+++ main.cpp
@@ -3,147 +3,17 @@
 #include <exception>
 #include <iostream>
 
-struct Cords {
-    size_t x1 = 0;
-    size_t x2 = 0;
-    size_t size = 0;
-};
-
-class Matrix {
-private:
-    std::vector<int> array;
-    size_t size_;
-public:
-    Matrix(size_t size) {
-        double k = std::log2(size);
-        if (int(k) != k) {
-            size_ = std::pow(2, int(k) + 1);
-        } else
-            size_ = std::pow(2, int(k));
-        array.resize(size_);
-    }
-
-    Matrix(Matrix& a, Matrix& b, Matrix& c, Matrix& d) {
-        size_ = a.size() * 2;
-        array.resize(size_);
-        for (size_t i = 0; i < a.size(); ++i)
-            for (size_t j = 0; j < a.size(); ++j) {
-                (*this)(i, j) = a(i, j);
-                (*this)(i, j) = b(i + a.size(), j);
-                (*this)(i, j) = a(i, j + a.size());
-                (*this)(i, j) = a(i + a.size(), j + a.size());
-            }
-    }
-
-    int & operator ()(size_t i, size_t j) {
-        return array[j * size_ + i];
-    }
-
-    int & operator() (size_t i, size_t j) const {
-        return array[j * size_ + i];
-    }
-
-    size_t size() {
-        return size_;
-    }
-
-    size_t size() const {
-        return size_;
-    }
-
-    friend Matrix operator* (Matrix& a, Matrix& b) {
-        Matrix c(a.size());
-        for (size_t i = 0; i < a.size(); ++i)
-            for (size_t j = 0; j < a.size(); ++j)
-                for (size_t k = 0; k < a.size(); ++k)
-                    c(i, j)+= a(k, j) * b(i, k);
-        return c;
-    }
-
-    Matrix operator+= (Matrix& a) {
-        for (size_t i = 0; i < a.size(); ++i)
-            for (size_t j = 0; j < a.size(); ++j)
-                (*this)(i, j) += a(i, j);
-        return *this;
-    }
-
-    friend Matrix operator+ (Matrix a, Matrix& b) {
-        a += b;
-        return a;
-    }
-
-    Matrix operator-= (Matrix& a) {
-        for (size_t i = 0; i < a.size(); ++i)
-            for (size_t j = 0; j < a.size(); ++j)
-                (*this)(i, j) -= a(i, j);
-        return *this;
-    }
-
-    friend Matrix operator- (Matrix a, Matrix& b) {
-        a -= b;
-        return a;
-    }
-};
-
-Matrix quarter(Matrix& a, int quarter) {
-    Matrix c(a.size() / 2);
-    int i, j, m, n;
-    if (quarter == 1) {
-        i = 0;
-        j = 0;
-    } else if (quarter == 2) {
-        i = a.size() / 2;
-        j = 0;
-    } else if (quarter == 3) {
-        i = 0;
-        j = a.size() / 2;
-    } else {
-        i = a.size() / 2;
-        j = a.size() / 2;
-    }
-    for (int l = 0; l < a.size() / 2; ++l)
-        for (int k = 0; k < a.size() / 2; ++k)
-            c(l, k) = a(i + l, j + k);
-    return c;
-}
-
-Matrix strassen(Matrix& a, Matrix& b) {
-    if (a.size() <= 64) {
-        Matrix c(a.size());
-        c = a * b;
-        return c;
-    } else
-    Matrix m1(a.size() / 2) = strassen(quarter(a, 1) + quarter(a, 4), quarter(b, 1) + quarter(b, 4));
-    Matrix m2(a.size() / 2) = strassen(quarter(a, 3) + quarter(a, 4), quarter(b, 1));
-    Matrix m3(a.size() / 2) = strassen(quarter(a, 1), quarter(b, 2) - quarter(b, 4));
-    Matrix m4(a.size() / 2) = strassen(quarter(a, 4), quarter(b, 3) - quarter(b, 1));
-    Matrix m5(a.size() / 2) = strassen(quarter(a, 1) + quarter(a, 2), quarter(b, 4));
-    Matrix m6(a.size() / 2) = strassen(quarter(a, 3) - quarter(a, 1), quarter(b, 1) + quarter(b, 2));
-    Matrix m7(a.size() / 2) = strassen(quarter(a, 2) - quarter(a, 4), quarter(b, 3) + quarter(b, 4));
-    Matrix c(m1 + m4 - m5 + m7, m3 + m5, m2 + m4, m1 - m2 + m3 + m6);
-    return c;
-};
+#include "Matrix.h"
 
 int main() {
     size_t n;
     std::cin >> n;
-    Matrix a(n), b(n);
-    for (size_t i = 0; i < n; ++i) {
-        for (size_t j = 0; j < n; ++j) {
-            int temp;
-            std::cin >> temp;
-            a(i, j) = temp;
-        }
-    }
-    for (size_t i = 0; i < n; ++i) {
-        for (size_t j = 0; j < n; ++j) {
-            int temp;
-            std::cin >> temp;
-            b(i, j) = temp;
-        }
-    }
-    Matrix c = strassen(a, b);
-    for (size_t i = 0; i < n; ++i)
-        for (size_t j = 0; j < n; ++j)
-            std::cout << c(i, j);
+    Matrix<int> a(n);
+    Matrix<int> b(n);
+    std::cin >> a >> b;
+
+    std::cout << a << std::endl << b << std::endl;
+
+    Matrix<int> c = a * b;
+    std::cout << c;
 }
